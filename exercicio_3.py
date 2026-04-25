b618117696044901592dda8157bcceb27527c5f4
sousadcarolina-hue <sousad.carolina@gmail.com>
2026-04-25 17:08:01 -0300

    Update exercicio_3.py

--- exercicio_3.py
+++ exercicio_3.py
@@ -17,3 +17,11 @@ Digite um número:
 
 Dica: Lembre do comando de resto da divisão inteira!
 """
+
+
+numero = int(input('Digite seu numero:  '))
+
+if numero % 2 == 0:
+  print ('Seu número é par')
+else:
+  print ('Seu número é impar')
